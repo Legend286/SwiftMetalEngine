be05e915a66f7c18a5f58fda88483b8eaa5abcb4
Legend286 <asagpol@gmail.com>
2022-10-19 16:13:49 +0100

    Update interpolation function library with algorithm source link.
    Title says all...

--- HelloMetal/InterpolationFunctions.swift
+++ HelloMetal/InterpolationFunctions.swift
@@ -5,6 +5,9 @@ func lerp(a: Float, b: Float, t: Float) -> Float
     return a + (b - a) * t
 }
 
+// This code was nice to come across, and provides a relatively okay smoothing
+// function when you don't need to accurately reach the target values.
+// https://www.construct.net/en/blogs/ashleys-blog-2/using-lerp-delta-time-924
 func smoothLerp(a: Float, b: Float, t: Float, f: Float) -> Float
 {
     return lerp(a: a, b: b, t: 1.0-pow(f,t))
